6e12fbc6734096f5d5a79c14623861a753a6e850
mohammad <mohammad.sharifi.baraftab@gmail.com>
2024-12-07 15:11:58 +0330

    initial commit

---
+++ digital_products/local_settings.py
@@ -0,0 +1,10 @@
+DEBUG = True
+DEVELOP = True
+ALLOWED_HOSTS = []
+
+DB_NAME = "digital_products"
+DB_USER = "root"
+DB_PASSWORD = "msh632825"
+DB_HOST = "127.0.0.1"
+
+BASE_URL  = "http://127.0.0.1:8000/"
